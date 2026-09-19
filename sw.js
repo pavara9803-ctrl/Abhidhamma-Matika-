cc7552cc53aa6e68de546a7a46dbde01201f2472
pavara9803-ctrl <pavaracitta9803@gmail.com>
2026-09-19 07:31:08 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,13 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('matika-store').then((cache) => {
-      return cache.addAll(['./', './index.html']);
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './launchericon-192x192.png',
+        './launchericon-512x512.png'
+      ]);
     })
   );
 });
